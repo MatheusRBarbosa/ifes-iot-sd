57ac4b747fd11de5f4ddf8d57275a9ff278200ae
Matheus Barbosa <matheuskleber@live.com>
2019-06-21 21:32:28 -0300

    Adicionando aplicacao

---
+++ aplicacao/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { ApplicationModule } from './app.module';
+
+async function bootstrap() {
+  const app: NestExpressApplication = await NestFactory.create(ApplicationModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'views/assets'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(3000);
+}
+bootstrap();
--- geraTemp.py
+++ geraTemp.py
@@ -4,7 +4,7 @@ import requests
 temperatura = input('Temperatura: ')
 umidade = input('Umidade: ')
 
-apiUrl = "http://localhost:3000/temperatura/{0}/umidade/{1}".format(temperatura, umidade)
+apiUrl = "http://localhost:3001/temperatura/{0}/umidade/{1}".format(temperatura, umidade)
 
 response = requests.post(apiUrl)
 
